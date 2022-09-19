56aed42a95af938495931ce0adc03db0272d4b67
Kristina123455 <kristinaneskovic9@gmail.com>
2022-09-20 00:14:16 +0200

    routes added

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
+//Route::resource('books', BookController::class);
+Route::get('/books/search/{name}', [BookController::class,'search']);
+Route::get('/books/{id}', [BookController::class,'show']);
+Route::get('/books', [BookController::class,'index']);
+Route::post('/register',[AuthController::class,'register']); 
+Route::post('/login',[AuthController::class,'login']); 
+
+//protected routes
+Route::group(['middleware'=>['auth:sanctum']], function () {
+    Route::post('/books',[BookController::class,'store']); 
+    Route::put('/books/{id}',[BookController::class,'update']); 
+    Route::delete('/books/{id}',[BookController::class,'destroy']); 
+    Route::post('/logout',[AuthController::class,'logout']); 
+    
+});
+
+Route::middleware('auth:api')->get('/user',function(Request $request){
     return $request->user();
 });
